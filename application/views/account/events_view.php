bcb1baad2826b35ee85b46e56fd0173d0909f83b
cuteporing <krishiavalencia@gmail.com>
2015-03-09 10:55:57 +0800

    LANGS

--- application/views/account/events_view.php
+++ application/views/account/events_view.php
@@ -19,29 +19,29 @@
     <?php if( isset($show_action_btn) ):?>
         <?php if( $show_action_btn == 'restrict' ):?>
             <div class="btn-group  no-print">
-                <button type="button" class="btn btn-default btn-flat">Action</button>
+                <button type="button" class="btn btn-default btn-flat"><?=$this->lang->line('lbl_action')?></button>
                 <button type="button" class="btn btn-default btn-flat dropdown-toggle" data-toggle="dropdown">
                     <span class="caret"></span>
-                    <span class="sr-only">Toggle Dropdown</span>
+                    <span class="sr-only"><?=$this->lang->line('lbl_toggle_drop')?></span>
                 </button>
                 <ul class="dropdown-menu" role="menu">
-                    <li><a href="<?=$btn_edit?>">Edit</a></li>
+                    <li><a href="<?=$btn_edit?>"><?=$this->lang->line('lbl_edit')?></a></li>
                 </ul>
             </div><br></br>
         <?php else: ?>
             <div class="btn-group  no-print">
-                <button type="button" class="btn btn-default btn-flat">Action</button>
+                <button type="button" class="btn btn-default btn-flat"><?=$this->lang->line('lbl_action')?></button>
                 <button type="button" class="btn btn-default btn-flat dropdown-toggle" data-toggle="dropdown">
                     <span class="caret"></span>
-                    <span class="sr-only">Toggle Dropdown</span>
+                    <span class="sr-only"><?=$this->lang->line('lbl_toggle_drop')?></span>
                 </button>
                 <ul class="dropdown-menu" role="menu">
-                    <li><a href="<?=$btn_edit?>">Edit</a></li>
-                    <li><a href="<?=$btn_approve?>" data-ajax="edit" data-ajax-confirm-msg="">Approve</a></li>
-                    <li><a href="<?=$btn_denied?>" data-ajax="edit" data-ajax-confirm-msg="">Denied</a></li>
-                    <li><a href="<?=$btn_revise?>" data-ajax="edit" data-ajax-confirm-msg="">For revision</a></li>
+                    <li><a href="<?=$btn_edit?>"><?=$this->lang->line('lbl_edit')?></a></li>
+                    <li><a href="<?=$btn_approve?>" data-ajax="edit" data-ajax-confirm-msg=""><?=$this->lang->line('lbl_approve')?></a></li>
+                    <li><a href="<?=$btn_denied?>" data-ajax="edit" data-ajax-confirm-msg=""><?=$this->lang->line('lbl_denied')?></a></li>
+                    <li><a href="<?=$btn_revise?>" data-ajax="edit" data-ajax-confirm-msg=""><?=$this->lang->line('lbl_for_revision')?></a></li>
                     <li class="divider"></li>
-                    <li><a href="<?=$btn_delete?>" data-ajax="delete" data-del-type="redirect"><i class="fa fa-trash-o"></i> Delete</a></li>
+                    <li><a href="<?=$btn_delete?>" data-ajax="delete" data-del-type="redirect"><i class="fa fa-trash-o"></i> <?=$this->lang->line('lbl_delete')?></a></li>
                 </ul>
             </div><br></br>
         <?php endif; ?>
@@ -64,30 +64,34 @@
                             <small>TIME:</small>&nbsp;<?=$result_event['time'] ?><br>
                             <small>VENUE:</small>&nbsp;<?=$result_event['location'] ?><br>
                         </p><br>
+                        <!-- CATEGORY -->
                         <p>
-                            <strong>CATEGORY</strong><br>
+                            <strong><?=$this->lang->line('lbl_CATEGORY')?></strong><br>
                             <?=$result_event['category_name'] ?>
                         </p>
+                        <!-- DESCRIPTION -->
                         <p>
-                            <strong>DESCRIPTION</strong><br>
+                            <strong><?=$this->lang->line('lbl_DESCRIPTION')?></strong><br>
                             <?php if( isset($result_desc) ): ?>
                                 <?php foreach ($result_desc as $description): ?>
                                     <p><?= $description['description'] ?></p>
                                 <?php endforeach; ?>
                             <?php else: ?>
-                                <small style="color: rgb(174, 174, 174);">-- No Description --</small>
+                                <small style="color: rgb(174, 174, 174);"><?=$this->lang->line('lbl_no_description')?></small>
                             <?php endif; ?>
                         </p>
+                        <!-- IN-CHARGE / PARTICIPANTS -->
                         <p>
-                            <strong>IN-CHARGE / PARTICIPANTS</strong><br>
+                            <strong><?=$this->lang->line('lbl_IN_CHARGE')?></strong><br>
                             <?php if( strlen($result_event['in_charge']) > 0 ): ?>
                                 <?=$result_event['in_charge'] ?>
                             <?php else: ?>
-                                <small style="color: rgb(174, 174, 174);">-- No in-charge personnel --</small>
+                                <small style="color: rgb(174, 174, 174);"><?=$this->lang->line('lbl_no_incharge')?></small>
                             <?php endif; ?>
                         </p>
+                        <!-- BENEFICIARY -->
                         <p>
-                            <strong>BENEFICIARY</strong><br>
+                            <strong><?=$this->lang->line('lbl_BENEFICIARY')?></strong><br>
                             <?php if( isset($result_beneficiary) ): ?>
                                 <ul style="list-style-type: disc; margin-left: 1.5em;">
                                     <?php foreach ($result_beneficiary as $beneficiary): ?>
@@ -95,34 +99,39 @@
                                     <?php endforeach; ?>
                                 </ul>
                             <?php else: ?>
-                                <small style="color: rgb(174, 174, 174);">-- No beneficiary -- </small>
+                                <small style="color: rgb(174, 174, 174);"><?=$this->lang->line('lbl_no_beneficiary')?></small>
                             <?php endif; ?>
                         </p>
+                        <!-- MATERIALS NEEDED -->
                         <p>
-                            <strong>MATERIALS NEEDED</strong><br>
+                            <strong><?=$this->lang->line('lbl_nMATERIALS_NEEDED')?></strong><br>
                             <?php if( strlen($result_event['materials_needed']) > 0 ): ?>
                                 <?=$result_event['materials_needed'] ?>
                             <?php else: ?>
-                                <small style="color: rgb(174, 174, 174);">-- No materials needed --</small>
+                                <small style="color: rgb(174, 174, 174);"><?=$this->lang->line('lbl_no_materials_needed')?></small>
                             <?php endif; ?>
                         </p>
+                        <!-- BUDGET -->
                         <p>
-                            <strong>BUDGET</strong><br>
+                            <strong><?=$this->lang->line('lbl_BUDGET')?></strong><br>
                             <?php if( strlen($result_event['budget']) > 0 ): ?>
                                 <?=$result_event['budget'] ?>
                             <?php else: ?>
-                                <small style="color: rgb(174, 174, 174);">-- No budget proposed --</small>
+                                <small style="color: rgb(174, 174, 174);"><?=$this->lang->line('lbl_no_budget_proposed')?></small>
                             <?php endif; ?>
                         </p>
+                        <!-- EXPECTED OUTPUT -->
                         <p>
-                            <strong>EXPECTED OUTPUT</strong><br>
+                            <strong><?=$this->lang->line('lbl_EXPECTED_OUTPUT')?></strong><br>
                             <?php if( strlen($result_event['expected_output']) > 0 ): ?>
                                 <?=$result_event['expected_output'] ?>
                             <?php else: ?>
-                                <small style="color: rgb(174, 174, 174);">-- No expected output --</small>
+                                <small style="color: rgb(174, 174, 174);"><?=$this->lang->line('lbl_no_expected_output')?></small>
                             <?php endif; ?>
                         </p><hr>
-                        <p class="text-center">Prepared by</p><br>
+
+                         <!-- PREPARED BY -->
+                        <p class="text-center"><?=$this->lang->line('lbl_prepared_by')?></p><br>
                         <p class="text-center">
                         <?php if( isset($result_owner) ): ?>
                             <?php foreach ($result_owner as $owner): ?>
@@ -136,7 +145,7 @@
                         <?php endif; ?>
                         </p><br><br>
 
-                        <p class="text-center">Approved by</p>
+                        <p class="text-center"><?=$this->lang->line('lbl_approved_by')?></p>
 
                         <?php $name = $owner['first_name'].' '.$owner['last_name'];?>
                         <?php if( isset($result_confirmation) ): ?>
@@ -144,15 +153,15 @@
                                         $name != $result_confirmation['cop_director'] ): ?>
                             <p class="text-center">
                                 <strong style="text-transform:uppercase;"><?=$result_confirmation['cop_director']?></strong><br>
-                                Cop Director
+                                <?=$this->lang->line('lbl_cop_director')?>
                             </p>
                             <?php endif; ?>
                             <br>
 
-                            <?php if( strlen($result_confirmation['sps_director']) > 0 ): ?>
+                        <?php if( strlen($result_confirmation['sps_director']) > 0 ): ?>
                             <p class="text-center">
                                 <strong style="text-transform:uppercase;"><?=$result_confirmation['sps_director']?></strong><br>
-                                SPS Director
+                                <?=$this->lang->line('lbl_sps_director')?>
                             </p>
                             <?php endif; ?>
                             <br>
--- system/language/english/labels_lang.php
+++ system/language/english/labels_lang.php
@@ -6,28 +6,45 @@ $lang['lbl_sub_logo'] = 'Community Outreach Program';
 $lang['lbl_about_us'] = 'About Us';
 $lang['lbl_accept'] = 'Accept';
 $lang['lbl_account_status'] = 'Account status';
+$lang['lbl_action'] = 'Action';
 $lang['lbl_add_beneficiary'] = 'Add beneficiary';
 $lang['lbl_add_member'] = 'Add member';
 $lang['lbl_admin'] = 'Admin';
 $lang['lbl_announcement'] = 'Announcement';
+$lang['lbl_approved_by'] = 'Approved by';
+$lang['lbl_BENEFICIARY'] = 'BENEFICIARY';
+$lang['lbl_BUDGET'] = 'BUDGET';
+$lang['lbl_CATEGORY'] = 'CATEGORY';
 $lang['lbl_contact_details'] = 'Contact details';
 $lang['lbl_contact_us'] = 'Contact Us';
+$lang['lbl_cop_director'] = 'COP Director';
 $lang['lbl_create_announcement'] = 'Create new announcement';
 $lang['lbl_create_events'] = 'Create an event proposal';
 $lang['lbl_create_user'] = 'Create user';
+$lang['lbl_DESCRIPTION'] = 'DESCRIPTION';
 $lang['lbl_edit_announcement'] = 'Edit announcement';
 $lang['lbl_edit_beneficiary'] = 'Edit beneficiary';
 $lang['lbl_edit_user'] = 'Edit user';
+$lang['lbl_edit_events'] = 'Edit event';
 $lang['lbl_event_description'] = 'Event Description';
 $lang['lbl_event_details'] = 'Event Details';
 $lang['lbl_event_proposal'] = 'Event proposal';
 $lang['lbl_event_status'] = 'Event status';
-$lang['lbl_edit_events'] = 'Edit event';
+$lang['lbl_EXPECTED_OUTPUT'] = 'EXPECTED OUTPUT';
 $lang['lbl_forgot_password'] = 'I forgot my password';
 $lang['lbl_forum'] = 'Forum';
 $lang['lbl_have_membership'] = 'I already have a membership';
 $lang['lbl_home'] = 'Home';
+$lang['lbl_IN_CHARGE'] = 'IN-CHARGE / PARTICIPANTS';
+$lang['lbl_MATERIALS_NEEDED'] = 'MATERIALS NEEDED';
+$lang['lbl_no_beneficiary'] = '-- No beneficiary --';
+$lang['lbl_no_budget_proposed'] = '-- No budget proposed --';
+$lang['lbl_no_description'] = '-- No Description --';
+$lang['lbl_no_expected_output'] = '-- No expected output --';
+$lang['lbl_no_incharge'] = '-- No in-charge personnel --';
+$lang['lbl_no_materials_needed'] = '-- No materials needed --';
 $lang['lbl_personal_info'] = 'Personal Information';
+$lang['lbl_prepared_by'] = 'Prepared by';
 $lang['lbl_register_member'] = 'Register a new membership';
 $lang['lbl_register_membership'] = 'Register New Membership';
 $lang['lbl_remember_me'] = 'Remember me';
@@ -36,8 +53,11 @@ $lang['lbl_seminars'] = 'Seminars';
 $lang['lbl_sign_in'] = 'Sign In';
 $lang['lbl_sign_me_in'] = 'Sign me in';
 $lang['lbl_sign_me_up'] = 'Sign me up';
+$lang['lbl_sps_director'] = 'SPS Director';
 $lang['lbl_reject'] = 'Reject';
 $lang['lbl_training'] = 'Training';
+$lang['lbl_toggle_drop'] = 'Toggle Dropdown';
+
 
 //FORM LABELS
 $lang['lbl_account_type'] = 'Account type';
@@ -74,15 +94,31 @@ $lang['lbl_user_type'] = 'User type';
 
 //BUTTONS
 $lang['lbl_add_photo'] = 'Add photo';
+$lang['lbl_approve'] = 'Approve';
 $lang['lbl_custom_album'] = 'Custom album';
 $lang['lbl_custom_event_album'] = 'Create event album';
+$lang['lbl_delete'] = 'Delete';
 $lang['lbl_delete_album'] = 'Delete album';
 $lang['lbl_delete_banner'] = 'Delete banner';
 $lang['lbl_delete_photo'] = 'Delete photo';
+$lang['lbl_denied'] = 'Denied';
+$lang['lbl_edit'] = 'Edit';
 $lang['lbl_edit_album'] = 'Edit album';
+$lang['lbl_for_revision'] = 'For revision';
 $lang['lbl_set_as_cover'] = 'Set as cover photo';
 $lang['lbl_view_gallery'] = 'View gallery';
 
 
 /* End of file labels.php */
 /* Location: ./system/language/english/labels.php */
+
+
+
+
+
+
+
+
+
+
+
