7317acde022405fafaf49ce87622fbcc9cb482f2
Cuteporing <krishiavalencia@gmail.com>
2015-05-11 16:53:01 +0800

    SQL DUMP FILE

--- cop_portal.sql
+++ cop_portal.sql
@@ -450,7 +450,7 @@ CREATE TABLE IF NOT EXISTS `cop_gallery` (
   `title` varchar(255) NOT NULL,
   `description` text,
   `date_entered` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `date_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `date_modified` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `slug` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
